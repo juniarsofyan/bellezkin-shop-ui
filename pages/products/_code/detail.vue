d6dad8dc375e7e1415ac28aadb9d2cdebbb9df11
oleguns <37364801+oleguns@users.noreply.github.com>
2019-10-11 16:34:18 +0700

    Yonip
    Detail.vue edit eutik

--- pages/products/_code/detail.vue
+++ pages/products/_code/detail.vue
@@ -81,11 +81,11 @@
 									<a :href="`https://api.whatsapp.com/send?phone=628112288142&text=Halo!%0ASaya%20ingin%20ingin%20konsultasi%20lebih%20lanjut%20mengenai%20produk%20Bellezkin%0ASource : ${url}`"
 										class="single_bc_button button" target="_blank">
 										<i class="fab fa-whatsapp logo_bc"></i>&nbsp;
-										Contact Beauty Consultant Bellezkin
+										Contact Beauty Consultant
 									</a>
 								</div>
 								
-								<p id="warn">&nbsp; Klik <i class="fas fa-arrow-up"></i> untuk langsung terhubung via WhatssApp</p>
+								<p id="warn">&nbsp; Klik <i class="fas fa-arrow-up"></i> untuk terhubung via WhatssApp</p>
 							</div>
 						</div>
 					</div>
